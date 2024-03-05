05ec28516c24112f110b4e7de955150b93f66bba
ryanef <ryanef39@gmail.com>
2024-03-04 19:12:01 -0500

    first

--- locals.tf
+++ locals.tf
@@ -16,29 +16,5 @@ locals {
       visibility = "public"
     }
 
-    tf-aws-ecs = {
-      name = "tf-aws-ecs"
-      owner = var.GITHUB_OWNER
-      description = "ecs"
-      gitignore_template = "Terraform"
-      visibility = "public"
-    }
-
-    terraform-aws-vpc = {
-      name = "terraform-aws-vpc"
-      owner = var.GITHUB_OWNER
-      description = "aws vpc"
-      gitignore_template = "Terraform"
-      visibility = "public"
-    }
-
-    tfe-aws-lb-automate = {
-      name = "tfe-aws-lb-automate"
-      owner = var.GITHUB_OWNER
-      description = "aws loadbalancer"
-      gitignore_template = "Terraform"
-      visibility = "public"
-    }
-
   }
 }
